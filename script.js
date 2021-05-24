fb1945871bb73ebe9728175f3724c60e7de427a5
Sarzy <adigwegodswillchinonso@gmail.com>
2021-05-24 18:41:45 +0100

    done

---
+++ script.js
@@ -0,0 +1,26 @@
+const header = document.getElementById("header");
+const title = document.getElementById("title");
+const excerpt = document.getElementById("excerpt");
+const profile_img = document.getElementById("profile_img");
+const name1 = document.getElementById("name");
+const date = document.getElementById("date");
+
+const animated_bgs = document.querySelectorAll(".animated-bg");
+const animated_bg_texts = document.querySelectorAll(".animated-bg-text");
+
+setTimeout(getData, 2500);
+
+function getData() {
+  header.innerHTML =
+    '<img src="https://images.unsplash.com/photo-1593642532973-d31b6557fa68?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" alt="" />';
+  title.innerHTML = "Lorem ipsum dolor sit amet";
+  excerpt.innerHTML =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis";
+  profile_img.innerHTML =
+    '<img src="https://images.unsplash.com/photo-1604344926618-f017932c780f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="" />';
+  name1.innerHTML = "John Doe";
+  date.innerHTML = "Oct 08, 2020";
+
+  animated_bgs.forEach((bg) => bg.classList.remove("animated-bg"));
+  animated_bg_texts.forEach((bg) => bg.classList.remove("animated-bg-text"));
+}
